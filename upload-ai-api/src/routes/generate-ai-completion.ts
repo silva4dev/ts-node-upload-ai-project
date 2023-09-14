cb57792e574cbde2a365c21117a85fc51766fab2
silva4dev <web4dev.lucas@outlook.com>
2023-09-14 00:24:21 -0300

    feat: add generate ai completion route

---
+++ upload-ai-api/src/routes/generate-ai-completion.ts
@@ -0,0 +1,47 @@
+import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+import { streamToResponse, OpenAIStream } from 'ai'
+import { prisma } from '../lib/prisma'
+import { openai } from '../lib/openai'
+
+export async function generateAiCompletionRoute(app: FastifyInstance) {
+  app.post('/ai/complete', async (request: FastifyRequest, reply: FastifyReply) => {
+    const bodySchema = z.object({
+      videoId: z.string().uuid(),
+      prompt: z.string(),
+      temperature: z.number().min(0).max(1).default(0.5),
+    })
+
+    const { videoId, prompt, temperature } = bodySchema.parse(request.body)
+
+    const video = await prisma.video.findUniqueOrThrow({
+      where: {
+        id: videoId
+      }
+    })
+
+    if (!video.transcription) {
+      return reply.status(400).send({ error: 'Video transcription was not generated yet.' })
+    }
+
+    const promptMessage = prompt.replace('{transcription}', video.transcription)
+
+    const response = await openai.chat.completions.create({
+      model: 'gpt-3.5-turbo-16k',
+      temperature,
+      messages: [
+        { role: 'user', content: promptMessage }
+      ],
+      stream: true,
+    })
+
+    const stream = OpenAIStream(response)
+
+    streamToResponse(stream, reply.raw, {
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+      }
+    })
+  })
+}
